491fadd064652c2fc291a45fa9957167fc3f4ed1
MaxianEdison <MaximilianEdison@gmail.com>
2023-04-15 05:18:18 +0330

    Solve typeo in helper.go

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -8,7 +8,7 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-func (app *applicayion) readIDParam(r *http.Request) (int64, error) {
+func (app *application) readIDParam(r *http.Request) (int64, error) {
 	params := httprouter.ParamsFromContext(r.Context())
 
 	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/BaseMax/MoviesGoAPI
 
 go 1.20
 
-require github.com/julienschmidt/httprouter v1.3.0 // indirect
+require github.com/julienschmidt/httprouter v1.3.0
